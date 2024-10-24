26b65b1fdedb7ed62e567292739339abf2f6ef4f
vinciusc7 <vinciusc7@gmail.com>
2024-10-24 06:21:40 -0300

    criado formulário de login e signup com formulário reativo

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,23 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   loginCard = true;
 
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLoginForm(): void {
+    console.log('Dados do formulário de login', this.loginForm.value);
+  }
+
+  onSubmitSignupForm(): void {
+    console.log('Dados do formulário de signup', this.signupForm.value);
+  }
 }
