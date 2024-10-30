3e17d48fe15980138d018a91760ac6833c64646c
vinciusc7 <vinciusc7@gmail.com>
2024-10-30 07:03:42 -0300

    criado lógica de autenticação e armazenamento do jwt nos cookies

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,5 +1,9 @@
+import { CookieService } from 'ngx-cookie-service';
+import { UserService } from './../../services/user/user.service';
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AuthRequest } from 'src/app/models/interfaces/user/AuthRequest';
+import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
 
 @Component({
   selector: 'app-home',
@@ -19,13 +23,39 @@ export class HomeComponent {
     email: ['', Validators.required],
     password: ['', Validators.required]
   })
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private cookieService: CookieService) {}
 
   onSubmitLoginForm(): void {
-    console.log('Dados do formulário de login', this.loginForm.value);
+    if(this.loginForm.value && this.loginForm.valid) {
+      this.userService.authUser(this.loginForm.value as AuthRequest)
+      .subscribe({
+        next: (response) => {
+          if(response){
+            this.cookieService.set('USER_INFO', response?.token);
+
+            this.loginForm.reset();
+          }
+        },
+        error: (err) => console.log(err)
+      });
+    }
   }
 
   onSubmitSignupForm(): void {
-    console.log('Dados do formulário de signup', this.signupForm.value);
+    if(this.signupForm.value && this.signupForm.valid){
+      this.userService.signupUser(this.signupForm.value as SignupUserRequest)
+        .subscribe({
+          next: (response) => {
+            if(response) {
+              alert('Usuário criado com sucesso');
+              this.signupForm.reset();
+              this.loginCard = true;
+            }
+          },
+          error: (error) => {
+            console.log(error);
+          }
+        })
+    }
   }
 }
